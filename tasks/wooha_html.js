f8c79876345454a6a88f6e7ea2d0d46f85d4bde4
Tao.z <zhangtao@codoon.com>
2015-07-23 19:16:13 +0800

    update

--- tasks/wooha_html.js
+++ tasks/wooha_html.js
@@ -139,7 +139,7 @@ module.exports = function (grunt) {
         return content;
     }
 
-    grunt.registerMultiTask('htmlbuild', "Grunt HTML Builder", function () {
+    grunt.registerMultiTask('wooha_html', "Grunt HTML Builder", function () {
         var params = this.options({
             beautify: true,
             data: {
